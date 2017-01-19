549ae5a02e5e9a9907dae2b4b7463804e5224ce5
Greg <gdmee181@gmail.com>
2017-01-19 19:10:32 +0000

    Add inline functions for bit manipulation: getbit setbit clrbit

--- present_ref/crypto.h
+++ present_ref/crypto.h
@@ -10,6 +10,22 @@
 #define CRYPTO_KEY_SIZE 10  // Present has 80-bit key
 #define CRYPTO_OUT_SIZE 8   // Present has 64-bit blocks
 
+// Define bit manipulation inline functions
+static inline uint8_t getbit(const uint8_t word, const uint8_t bit) {
+	return (word >> bit) & 0x1;
+	// or, if return value != 0 means: bit set
+	// return (word & (1 << bit));
+}
+
+static inline void setbit(const uint8_t* word, const uinst8_t bit) {
+	*word |= (1  << bit);
+}
+
+static inline void clrbit(const uint8_t* word, const uint8_t bit) {
+	*word &= ~(1 << bit);
+}
+
+
 // The function to test
 void crypto_func(uint8_t pt[CRYPTO_IN_SIZE], uint8_t key[CRYPTO_KEY_SIZE]);
 
